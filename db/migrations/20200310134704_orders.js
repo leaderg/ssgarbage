7d38c1cb7de002fcc95910698da8f7129511365f
Greg <leader.g@gmail.com>
2021-02-15 21:04:01 -0700

    Adding charges migration and adding range to values allowed in db.

--- db/migrations/20200310134704_orders.js
+++ db/migrations/20200310134704_orders.js
@@ -5,10 +5,10 @@ exports.up = function(knex) {
     t.bigInteger('customer_id').unsigned().index().references('id').inTable('customers');
     t.bigInteger('employee_id').unsigned().index().references('id').inTable('employees');
     t.timestamp('last_visited').defaultTo(knex.fn.now())
-    t.decimal('discount', 12, 4). defaultTo(0); // In cents
-    t.decimal('subtotal', 12, 4).defaultTo(0); // In Cents
-    t.decimal('tax', 12, 4).defaultTo(0); // In Cents
-    t.decimal('total', 12, 4).defaultTo(0); // In Cents
+    t.decimal('discount', 16, 4). defaultTo(0); // In cents
+    t.decimal('subtotal', 16, 4).defaultTo(0); // In Cents
+    t.decimal('tax', 16, 4).defaultTo(0); // In Cents
+    t.decimal('total', 16, 4).defaultTo(0); // In Cents
   });
 };
 
--- db/migrations/20200314212459_payment-table.js
+++ db/migrations/20200314212459_payment-table.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('payments', function(t) {
     t.increments('id').unsigned().primary();
     t.bigInteger('order_id').unsigned().index().references('id').inTable('orders');
-    t.decimal('amount', 12, 4);
+    t.decimal('amount', 16, 4);
     t.string('payment_method');
   });
 };
